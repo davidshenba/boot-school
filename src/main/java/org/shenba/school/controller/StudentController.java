6857a0f2fc360762b2a00af2f1076bc6374dcb7a
David P Shenba <davidpshenba@gmail.com>
2020-02-20 17:07:12 +0530

    added thymleaf and a basic page

--- src/main/java/org/shenba/school/controller/StudentController.java
+++ src/main/java/org/shenba/school/controller/StudentController.java
@@ -8,19 +8,20 @@ import org.shenba.school.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author shenba
  *
  */
-@RestController
+@Controller
 @RequestMapping("/students")
 public class StudentController {
 	
@@ -31,8 +32,9 @@ public class StudentController {
 	 * @return
 	 */
 	@GetMapping
-	public List<Student> getAllStudents() {
-		return studentService.getStudents();
+	public String getAllStudents(Model model) {
+		model.addAttribute("students", studentService.getStudents());
+		return "show-students";
 	}
 	
 	/**
